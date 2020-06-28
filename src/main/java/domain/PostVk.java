3631e49b4c8e08eace9c4e2579543bf05c3d683d
tester <mgnfirter@gmail.com>
2020-06-28 18:51:34 +0400

    Version 1

---
+++ src/main/java/domain/PostVk.java
@@ -0,0 +1,19 @@
+package domain;
+
+public class PostVk {
+    private String id;
+    private String name;
+    private String author;
+    private String image;
+    private String imageUrl;
+    private String date;
+    private String commentsInfo;
+    private String repostsInfo;
+    private int repostsQuantity;
+    private int likesQuantity;
+    private int viewsQuantity;
+    private String report;
+    private boolean bookmark;
+
+
+}
